f9fb4f805760bb8b1224f95924e6f64ba91af31d
nejohnson2 <nejohnson2@gmail.com>
2013-04-22 19:59:24 -0400

    adding more jquery

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,9 +18,15 @@ jQuery(document).ready(function() {
 	}
 	
 	drawMap();
+	drawEverything();
 
 });
-
+function hideSidebar(){
+	$("#sidebar").hide();
+}
+function showSidebar(){
+	$("#sidebar").show();
+}
 // binds a map marker and infoWindow together on click
 var bindInfoWindow = function(marker, map, infowindow, html) {
 
@@ -53,6 +59,7 @@ function drawMap(){
 
 
 function searchLandfills(){
+	drawMap();
 	var results = [];
 
 	var something = $("#formSearch").val();
@@ -70,7 +77,7 @@ function searchLandfills(){
 	    
 	    $.each(data, function(key, value){
 
-		    if(patt.test(value.Facility_Name) == true  && value.LRT != ""){
+		    if(patt.test(value.Facility_Name) == true  && value.LRTLat != ""){
     
 		    	// check for latlng and calculate distance 
 			    if(value.LRTLat != ""){
@@ -177,6 +184,146 @@ function searchLandfills(){
 		        var latLng = new google.maps.LatLng(results[a].value.LRTLat, results[a].value.LRTLon);		        
 
 		        if(thisMarker.title == thisId){
+
+	                    map.panTo(latLng);
+	                    map.setZoom(15)
+
+			        thisMarker.setAnimation(google.maps.Animation.BOUNCE);
+		        } else{
+			        thisMarker.setAnimation(null);
+		        }	
+		    }		    			    
+
+		});
+		$("#sidebar").show();	
+	    
+    	var landfill_length = "<p>" + results.length + "</p>";	    	
+		$("#landfill_length").html(landfill_length);
+
+	});
+
+}
+
+function drawEverything(){
+	var results = [];
+
+    //caluculate distance to landfills
+	var currentPos = latlng.toString();
+	currentPos = currentPos.replace("(","");
+	currentPos = currentPos.replace(")","");
+	currentPos = currentPos.split(",");
+	console.log(currentPos);
+		
+    $.getJSON('epa.json', function(data) {
+	    
+	    $.each(data, function(key, value){
+
+		    if(value.LRTLat != ""){
+
+			    var dist = calculateDistance(currentPos[0], currentPos[1], value.LRTLat, value.LRTLon);				    
+	        		    
+	    	    var tmpLatLng = new google.maps.LatLng(value.LRTLat, value.LRTLon);
+					
+				//create landfill marker	
+				var marker = new google.maps.Marker({
+				    map: map,
+				    position: tmpLatLng,
+				    title: value.UniqueID,
+				    icon: markerIconZero
+				});	
+				
+				
+				var infowindow =  new google.maps.InfoWindow({
+					content: ''
+				});
+				
+				var contentInfo = '<div id="content">'+
+					'<h3>' + value.Facility_Name + '</h3>' + 
+					'<div>' + value.Facility_Street + " " + value.Facility_City + ", " + value.Facility_State + ", " + value.Facility_ZIP_Code + '</div>' +
+					'<ul><li>Days since last inspection : ' + value.DaysSinceLastInspection + '</li>' +
+					
+					'<li> Alleged significat violations : ' + value.AllegedCurrentSignificantViolations + '</li>' +
+					
+					'<a href="/home">View Site</a>' + 
+					'</ul>'	
+					
+					
+				bindInfoWindow(marker, map, infowindow, contentInfo);		
+				
+				var result = {
+			    	value : value,
+			    	dist : Number(dist),
+			    	marker : marker
+		    	};
+		    	
+		    	results.push(result);	
+			    
+			    //remove markers when you click elsewhere
+			    google.maps.event.addListener(map, 'click', function(){
+			        infowindow.close();
+			    });			    
+		    }
+		    
+	    });
+	    
+	    /********************  Sidebar HTML  **************************/
+	    
+	    //sort the top ten and put closest on top
+	    results.sort(function(a, b) {
+		    return a["dist"] - b["dist"];
+		});	
+
+		results = results.slice(0,50);
+		results = results.reverse();	
+		
+	
+		for(i = 0; i < results.length; i++){
+
+			//create html for sidebar
+			var active = ""
+			if(results[i].value.CAAOperatingStatus != "active"){ active = "inactive"; }else{ active = results[i].value.CAAOperatingStatus; }
+			
+			var landfillHTML = "<li landfill_id=\"" + results[i].value.UniqueID + "\"><p><strong>" + results[i].value.Facility_Name + "</p></strong><p>" + 
+				results[i].dist + "mi" + " - " + 
+				active + "</li>" + landfillHTML;			
+		}
+		
+		$(".loading").remove();
+		$("#landfill-locations").html(landfillHTML);	
+		
+		
+	    /********************  Bounce Animation  **************************/		
+		
+		$("#landfill-locations li").on('mouseover', function(){
+			
+		    var thisId = $(this).attr("landfill_id"); // get id from sidebar
+		    
+		    for(a = 0; a < results.length; a++){
+		        var thisMarker = results[a].marker
+		        var latLng = new google.maps.LatLng(results[a].value.LRTLat, results[a].value.LRTLon);		        
+		
+		        if(thisMarker.title == thisId){
+			        thisMarker.setAnimation(google.maps.Animation.BOUNCE);
+		        } else{
+			        thisMarker.setAnimation(null);
+		        }
+		        
+		        
+		    }
+		});		
+
+		
+
+	    /********************  Zoom Animation  **************************/	
+		
+		$("#landfill-locations li").on('click', function(){
+	        var thisId = $(this).attr("landfill_id"); // get id from sidebar
+	        
+	        for(a = 0; a < results.length; a++){
+		        var thisMarker = results[a].marker
+		        var latLng = new google.maps.LatLng(results[a].value.LRTLat, results[a].value.LRTLon);		        
+
+		        if(thisMarker.title == thisId){
 	                if (map.getBounds().contains(latLng) == false){
 	                    map.panTo(latLng);
 	                }
@@ -194,7 +341,12 @@ function searchLandfills(){
 
 	});
 
+
+	
 }
+
+
+
 	    /********************  Location Math  **************************/	
 
 // calculate distance from current locaion 
